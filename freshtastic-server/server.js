b659cbb5ec5eaf882e4ac557b95568757e4b2be3
Vishal N <vishalnani1999@gmail.com>
2022-06-18 08:29:05 +0530

    Loggers and test endpoint

--- freshtastic-server/server.js
+++ freshtastic-server/server.js
@@ -4,6 +4,17 @@ require('dotenv').config()
 //Express application
 const server = express()
 
+/* Logging incoming traffic */
+server.use((request, response, next) => {
+    console.log(request.path, request.method)
+    next()
+})
+
+/* Plain GET request */
+server.get('/', (req, res) => {
+    res.json({message: 'Hello from the other side!'})
+})
+
 /* Setting up server port */
 const port = process.env.SERVER_PORT
 server.listen(port, () => {
